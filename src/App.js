2f65f24b8dfe409e28a40d757fa6423e688b30d0
cjlozada <cjlozada99@gmail.com>
2022-02-01 12:59:47 -0800

    Primera version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import List from './List/List';
+import Menu from './Menu/Menu.js';
+import './Menu/Menu.css';
+import './List/List.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Nada más por si acaso.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+
+    constructor(props){
+        super(props);
+
+        this.state ={
+          books:[
+            {id:0, rating:1, title: 'Hola Mundo 1'},
+            {id:1, rating:2, title: 'Hola Mundo 2: El regreso'},
+            {id:2, rating:3, title: 'Hola Mundo 3: Con Venganza'},
+            {id:3, rating:4, title: 'Hola Mundo 4: El Reboot'},
+          ]
+        };
+    }
+
+  render(){
+    return (
+    <div className="app">
+      <Menu title="Primera prueba"/>
+      <List items={this.state.books} />
     </div>
-  );
+    );
+  }
+
+
 }
 
 export default App;
---
+++ src/List/List.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Item from "./Item";
+
+function List(props){
+    return(
+        <div className="list">
+            {
+                props.items.map(item =>
+                <Item
+                    key={item.id}
+                    title={item.title}
+                    id={item.id}
+                    rating={item.rating} />
+                )
+            }
+        </div>
+    );
+}
+
+export default List;
